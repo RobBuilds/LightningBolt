62a9361c9fbcaed9985a65ea5253e4a4fc3f1909
WalkerB20 <bradenwalk24@gmail.com>
2024-02-07 11:22:54 -0500

    added Dockerfiles (template) and docker-compose.yml (template)

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NavigationMenu from './components/NavBar';
-import { AboutPage, ContactPage } from './components'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavigationMenu from './src/components/NavBar';
+import AboutPage from './src/components/AboutPage'
+import ContactPage from './src/components/ContactPage'
 
 function App() {
   return (
     <Router>
     <NavigationMenu />
-    <Switch>
+    <Routes>
       <Route path="/about" component={AboutPage} />
       <Route path="/contact" component={ContactPage} />
       {/* Add more routes as needed */}
       <Route path="/" exact> {/* Default or Home page content here */}</Route>
-    </Switch>
+    </Routes>
   </Router>
   );
 }
--- app/src/components/DataFetch.js
+++ app/src/components/DataFetch.js
@@ -1,5 +1,5 @@
-async function dataFetch(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-}
+//async function dataFetch(url) {
+  //  const response = await fetch(url);
+  //  const data = await response.json();
+  //  return data;
+//}
