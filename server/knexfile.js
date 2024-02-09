55df8dfa7f7412f9b919f6302c29ba940423364e
WalkerB20 <bradenwalk24@gmail.com>
2024-02-09 15:27:51 -0500

    lots of changes for Docker components and created functional database that, in theory, should be able to work with the APIs. all three stacks should be able to share information with one another now through Docker networks

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,54 +1,45 @@
-// Update with your config settings.
 require('dotenv').config();
-const connectionString = process.env.DB_CONNECTION_STRING;
 
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
-
   development: {
-    client: '',
+    client: 'pg',
     connection: {
-      host: '',
-      port: ,
-      user: '',
-      password: '',
-      database: 'spiderfootdb'
-    }
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    migrations: {
+      directory: __dirname + '/migrations',
+    },
   },
 
   staging: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: __dirname + '/migrations',
+    },
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      directory: __dirname + '/migrations',
+    },
+  },
 };
